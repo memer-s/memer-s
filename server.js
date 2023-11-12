55a5553aaf0033ab5985f1fde970324c6c5665d7
memer-s <mimeremim@gmail.com>
2023-11-12 01:25:10 +0100

    Add other files

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 
 
+app.get("/gol/:name", (req, res) => {
+	res.sendFile(__dirname+"/gol/"+req.params["name"]);
+})
+
 app.get("/", (req, res) => {
 	res.sendFile(__dirname+"/index.html");
 })
