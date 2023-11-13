d5a8cc9d7776bdba2f695f3267554540429029b8
memer <mimeremim@gmail.com>
2023-11-13 09:12:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
 
-
-app.get("/gol/:name", (req, res) => {
-	res.sendFile(__dirname+"/gol/"+req.params["name"]);
-})
-
 app.get("/", (req, res) => {
 	res.sendFile(__dirname+"/index.html");
 })
 
 app.get("/style.css", (req, res) => {
 	res.sendFile(__dirname+"/style.css");
+})
+
+app.get("/gol/:name", (req, res) => {
+	res.sendFile(__dirname+"/gol/"+req.params["name"]);
 })
 
 app.listen(8080);
